e4548905ca9cbe65408e9de2128bf4ff98304aa8
SophiGreen <phiainfinity@gmail.com>
2025-04-27 21:09:34 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-// STUDENT NAME:
-// DATE: 
+// STUDENT NAME: Sophia
+// DATE: 04/27/25
 
 public class Main 
 {
@@ -7,6 +7,17 @@ public class Main
 	{
 
 		/***** DECLARATION SECTION *****/
+		int monthToday, dayToday, yearToday, jdnToday;
+		int monthBday, dayBday, yearBday, jdnBday;
+		int diff, approxAge;
+		int a, m, y;
+
+		monthToday = 4;
+		dayToday = 27;
+		yearToday = 2025;
+		monthBday = 8;
+		dayBday = 26;
+		yearBday = 1918;
 
 		/***** INITIALIZATION SECTION *****/
 		
@@ -14,8 +25,25 @@ public class Main
 		// N/A (no input for this lab)
 		
 		/***** CALCULATION & PROCESSING SECTION *****/
-
+		a = (14 - monthToday) / 12;
+		m = monthToday + 12 * a - 3;
+		y = yearToday + 4800 - a;
+		jdnToday =  dayToday + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
 		
+		a = (14 - monthBday) / 12;
+		m = monthBday + 12 * a - 3;
+		y = yearBday + 4800 - a;
+		jdnBday =  dayBday + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+
+
+		diff = jdnToday - jdnBday;
+		approxAge = diff / 365;
+
 		/***** OUTPUT SECTION *****/
+		System.out.println("Julian Day Number for today's date, " + monthToday + "/" + dayToday + "/" + yearToday + ", is " + jdnBday);
+		System.out.println();
+		System.out.println("Julian Day Number for birthday, " + monthBday + "/" +  dayBday + "/" + yearBday + ", is " + jdnBday);
+		System.out.println();
+		System.out.println("The differenve in days is " + diff + ", which makes you approximately " + approxAge + " years old!"); 
   }
 }
